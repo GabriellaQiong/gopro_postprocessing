8c58bf3e2412c2819ec1d1369c0d3dd734f7f6b7
GabriellaQiong <qiong@seas.upenn.edu>
2014-02-23 22:00:44 -0500

    add rearrange matlab codes

---
+++ rearrange/script_rearrange_files.m
@@ -0,0 +1,37 @@
+% Run Script for rearrangin files
+% Written by Qiong Wang at University of Pennsylvania
+% 02/23/2014
+
+%% Clear up
+clear all;
+close all;
+clc;
+
+%% Parameters
+verbose  = false;                             % Whether to show the details
+
+%% Path
+scriptDir = fileparts(mfilename('fullpath'));
+dataDir   = '/media/Gabriella/GoPro-20140217-calib_data/images/';
+addpath(genpath(scriptDir));
+
+%% Rename files
+% Remove . and .. + find directories
+dirData = dir(dataDir);
+isDir   = [dirData(:).isdir];                 % returns logical vector
+dirName = {dirData(isDir).name}';
+dirName(ismember(dirName,{'.','..'})) = [];
+numCams = length(dirName);
+
+% Rename
+for camIdx = 1 : numCams
+    camDir  = fullfile(dataDir, char(dirName(camIdx)));
+    dirCam  = dir(camDir);
+    imgName = {dirCam(:).name}';
+    imgName(ismember(imgName,{'.','..'})) = [];
+    numImgs = length(imgName);
+    for imgIdx = 1 : numImgs
+        printf('Processing camera #%d frame #%d ...\n', camIdx, imgIdx);
+        movefile(fullfile(camDir, char(imgName(imgIdx))), fullfile(camDir, sprintf('camera%02d_%05d.bmp', camIdx, imgIdx)));
+    end
+end
